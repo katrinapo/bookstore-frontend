5ab90dfde5a00508b6d49d2b2a9f05746daf2a98
singmant <singmant@sheridancollege.ca>
2021-05-11 17:53:10 -0400

    made changes to bookList

--- src/app/booklist/booklist.component.ts
+++ src/app/booklist/booklist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../bookinventory/book';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-booklist',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booklist.component.css']
 })
 export class BooklistComponent implements OnInit {
-
-  constructor() { }
+  bookList: Book[];
+  constructor(private bookServ: BookService) { }
 
   ngOnInit(): void {
+    this.bookServ.getAllBooks().subscribe(
+      response => {
+        console.log(response);
+        this.bookList=response;
+      }
+    )
   }
 
 }
